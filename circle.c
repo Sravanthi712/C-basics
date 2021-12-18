fb0b098ecf08d1ce417f06af5eea5ec19bc9006d
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2021-12-18 15:23:53 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float area ,perimeter,r;
+	scanf("%f",&r);
+	area=3.14*r*r;
+	perimeter=2*3.14*r;
+	printf("the area of acircle  is %.2f",area);
+	printf("\nthe perimeter of a circle is %.2f",perimeter);
+}
