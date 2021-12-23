3de2394d7ddc569eedac4d744d7e586c3f590bfe
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2021-12-23 15:20:06 +0530

    Add files via upload

---
+++ arthmetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int x,y,sum,dif,pro,div,mod;
+	printf("enter two numbers");
+	scanf("%d%d",&x,&y);
+	sum=x+y;
+	printf("%d",sum);
+	dif=x-y;
+	printf("\n%d",dif);
+	pro=x*y;
+	printf("\n%d",pro);
+	div=x/y;
+	printf("\n%d",div);
+	mod=x%y;
+	printf("\nn%d",mod);
+	
+
+}
