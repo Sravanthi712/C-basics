0a33708bcc093b56d51f43978f19ee82e1bc350d
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-02-19 19:19:37 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int n,r,sum=0,temp;
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		r=n%10;
+		sum=sum*10+r;
+		n=n/10;
+	}
+	if(temp==sum)
+	{
+		printf("%d is a palindrome",temp);
+	}
+	else
+	{
+		printf("not a palindrome");
+	}
+}
